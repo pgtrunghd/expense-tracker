aa9cd1b3d61ab36116629a0696b839feb90e9b19
Trung Bs <trungpg@bytesoft.net>
2024-10-22 10:32:26 +0700

    update navigation

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -14,14 +14,14 @@ export const Navigation = () => {
 
   return (
     <section className="sticky inset-x-0 bottom-0 z-20 block border-t bg-background/60 shadow-md backdrop-blur md:hidden">
-      <ul className="relative flex h-full items-center justify-evenly">
+      <ul className="relative flex h-full items-center justify-evenly pt-2 pb-5">
         {navListMobile.map((item) =>
           item.type === "link" ? (
             <Link
               href={item.path}
               key={item.path}
               className={cn(
-                "flex flex-1 flex-col items-center justify-center py-2 text-zinc-400 duration-200 active:scale-90",
+                "flex flex-1 flex-col items-center justify-center text-zinc-400 duration-200 active:scale-90",
                 item.path === pathname ? "text-primary" : "",
               )}
             >
